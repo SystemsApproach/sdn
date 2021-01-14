f99f0d780fad337a1001e96524bb8ab6ff699e1a
Bruce Davie <3101026+drbruced12@users.noreply.github.com>
2021-01-14 15:51:49 +1100

    analytics update

--- conf.py
+++ conf.py
@@ -127,7 +127,7 @@ html_theme = 'sphinx_rtd_theme'
 #
 html_theme_options = {
   'prev_next_buttons_location': 'both',
-  'analytics_id': 'G-9C6ZHKMTSM'
+  'analytics_id': 'G-QLSP3FJWGT'
   }
 
 # Add any paths that contain custom static files (such as style sheets) here,
